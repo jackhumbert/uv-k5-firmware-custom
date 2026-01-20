48fdec667bc96d1450bce58a2a89b46986fb4813
Jack Humbert <jack.humb@gmail.com>
2026-01-20 16:01:01 -0500

    tone & transmit working
    Signed-off-by: Jack Humbert <jack.humb@gmail.com>

--- cw.c
+++ cw.c
@@ -25,6 +25,65 @@
 #include "functions.h"
 #include "debugging.h"
 #include "radio.h"
+#include "app/app.h"
+
+// uint32_t values[26] = {
+// 0b011101, // ".-",   // A
+// 0b0101010111, // "-...", // B
+// 0b010111010111, // "-.-.", // C
+// 0b01010111, // "-..",  // D
+// 0b01, // ".",    // E
+// 0b0101110101, // "..-.", // F
+// 0b0101110111, // "--.",  // G
+// 0b01010101, // "....", // H
+// 0b0101, // "..",   // I
+// 0b01110111011101, // ".---", // J
+// 0b0111010111, // "-.-",  // K
+// 0b0101011101, // ".-..", // L
+// 0b01110111, // "--",   // M
+// 0b010111, // "-.",   // N
+// 0b011101110111, // "---",  // O
+// 0b010111011101, // ".--.", // P
+// 0b01110101110111, // "--.-", // Q
+// 0b01011101, // ".-.",  // R
+// 0b010101, // "...",  // S
+// 0b0111, // "-",    // T
+// 0b01110101, // "..-",  // U
+// 0b0111010101, // "...-", // V
+// 0b0111011101, // ".--",  // W
+// 0b011101010111, // "-..-", // X
+// 0b01110111010111, // "-.--", // Y
+// 0b010101110111, // "--..", // Z
+// };
+
+// uint32_t inv_values[26] = {
+// 1, // ".",    // E
+// 5, // "..",   // I
+// 7, // "-",    // T
+// 21, // "...",  // S
+// 23, // "-.",   // N
+// 29, // ".-",   // A
+// 85, // "....", // H
+// 87, // "-..",  // D
+// 93, // ".-.",  // R
+// 117, // "..-",  // U
+// 119, // "--",   // M
+// 1399, // "--..", // Z
+// 1495, // "-.-.", // C
+// 1501, // ".--.", // P
+// 1879, // "-..-", // X
+// 1911, // "---",  // O
+// 343, // "-...", // B
+// 349, // ".-..", // L
+// 373, // "..-.", // F
+// 375, // "--.",  // G
+// 469, // "...-", // V
+// 471, // "-.-",  // K
+// 477, // ".--",  // W
+// 7543, // "--.-", // Q
+// 7639, // "-.--", // Y
+// 7645, // ".---", // J
+// };
 
 CWState_t gCWState = CW_INPUT_DISABLED;
 bool gSoundPlaying = false;
@@ -55,6 +114,10 @@ __inline uint16_t scale_freq(const uint16_t freq)
     return (((uint32_t)freq * 1353245u) + (1u << 16)) >> 17;   // with rounding
 }
 
+#define CW_TONE_TUNING_GAIN     66u
+#define CW_TONE_FREQ_MULTIPLIER 1353245u
+#define CW_SETUP_DELAY_MS       50
+
 static void CW_EnablePulse() {
 
     // BK4819_DisableDTMF();
@@ -64,8 +127,8 @@ static void CW_EnablePulse() {
     // LogUart("Start BEEP\n");
     AUDIO_AudioPathOff();
 
-    if (gCurrentFunction == FUNCTION_POWER_SAVE && gRxIdleMode)
-        BK4819_RX_TurnOn();
+    // if (gCurrentFunction == FUNCTION_POWER_SAVE && gRxIdleMode)
+    //     BK4819_RX_TurnOn();
 
     // SYSTEM_DelayMs(20);
 
@@ -73,7 +136,7 @@ static void CW_EnablePulse() {
 
     // BK4819_WriteRegister(BK4819_REG_70, BK4819_REG_70_ENABLE_TONE1 | ((1 & 0x7f) << BK4819_REG_70_SHIFT_TONE1_TUNING_GAIN));
     
-    BK4819_PlayTone(700, true); // false is a little lopassy
+    // BK4819_PlayTone(700, true); // false is a little lopassy
 
     // BK4819_EnterTxMute();
 
@@ -86,22 +149,101 @@ static void CW_EnablePulse() {
     // BK4819_EnableTXLink();
 
     // SYSTEM_DelayMs(2);
+
+    RADIO_PrepareTX();
+
+    BK4819_EnterTxMute();
+    // gets rid of harmonics on TX
+    BK4819_WriteRegister(BK4819_REG_47, (6u << 12) | (BK4819_AF_BEEP << 8) | (1u << 6) | 1);
+
+    // BK4819_SetAF(BK4819_AF_BEEP);
+    // BK4819_SetAF(BK4819_AF_AM);
+
+    // BK4819_WriteRegister(BK4819_REG_70, BK4819_REG_70_ENABLE_TONE1 | (CW_TONE_TUNING_GAIN << BK4819_REG_70_SHIFT_TONE1_TUNING_GAIN));
+    BK4819_WriteRegister(BK4819_REG_70, BK4819_REG_70_ENABLE_TONE1 | (0 << BK4819_REG_70_SHIFT_TONE1_TUNING_GAIN));
+    // BK4819_WriteRegister(BK4819_REG_70, BK4819_REG_70_ENABLE_TONE2 | (50 << BK4819_REG_70_SHIFT_TONE2_TUNING_GAIN));
+    // BK4819_WriteRegister(BK4819_REG_71, (((uint32_t)gCWSettings.tone_hz * CW_TONE_FREQ_MULTIPLIER) + (1u << 16)) >> 17);
+    BK4819_WriteRegister(BK4819_REG_71, scale_freq(700));
+    // BK4819_WriteRegister(BK4819_REG_72, scale_freq(700));
+    // BK4819_EnableTXLink();
+    // SYSTEM_DelayMs(CW_SETUP_DELAY_MS);
+
+    // 3360 Hz width with tone
+    // 1600 HZ width without tone
+
+    BK4819_WriteRegister(BK4819_REG_30,
+        BK4819_REG_30_ENABLE_VCO_CALIB |
+        BK4819_REG_30_ENABLE_UNKNOWN   |
+        BK4819_REG_30_DISABLE_RX_LINK  |
+        BK4819_REG_30_DISABLE_AF_DAC    | // disable beeps
+        // BK4819_REG_30_ENABLE_AF_DAC    | // enable beeps
+        BK4819_REG_30_ENABLE_DISC_MODE |
+        BK4819_REG_30_ENABLE_PLL_VCO   |
+        // BK4819_REG_30_ENABLE_PA_GAIN   |
+        BK4819_REG_30_DISABLE_PA_GAIN   | // disable PA
+        BK4819_REG_30_DISABLE_MIC_ADC  |
+        BK4819_REG_30_ENABLE_TX_DSP    |
+        // BK4819_REG_30_DISABLE_TX_DSP    | // disables TX
+        BK4819_REG_30_DISABLE_RX_DSP);
+
+    // might only affect RX
+    // BK4819_WriteRegister(BK4819_REG_43, (0b000 << 12) | (0b000 << 9) | (0b001 << 6) | (0b01 << 4) | (0b0 << 2));
     
+    BK4819_SetupPowerAmplifier(gCurrentVfo->TXP_CalculatedSetting, gCurrentVfo->pTX->Frequency);
     AUDIO_AudioPathOn();
 
-    // BK4819_ExitTxMute();
+    BK4819_ExitTxMute();
 
     gSoundPlaying = true;
 }
 
 static void CW_StartPulse() {
-    BK4819_ExitTxMute();
+    // BK4819_ExitTxMute();
     BK4819_ToggleGpioOut(BK4819_GPIO5_PIN1_RED, true);
+    
+    // BK4819_SetupPowerAmplifier(gCurrentVfo->TXP_CalculatedSetting, gCurrentVfo->pTX->Frequency);
+
+    // AUDIO_SetVoiceID(0, VOICE_ID_2_TONE);
+    // AUDIO_PlaySingleVoice(0);
+    
+    BK4819_WriteRegister(BK4819_REG_30,
+        BK4819_REG_30_ENABLE_VCO_CALIB |
+        BK4819_REG_30_ENABLE_UNKNOWN   |
+        BK4819_REG_30_DISABLE_RX_LINK  |
+        // BK4819_REG_30_DISABLE_AF_DAC    |
+        BK4819_REG_30_ENABLE_AF_DAC    | // enable beep
+        BK4819_REG_30_ENABLE_DISC_MODE |
+        BK4819_REG_30_ENABLE_PLL_VCO   |
+        BK4819_REG_30_ENABLE_PA_GAIN   | // enable PA
+        // BK4819_REG_30_DISABLE_PA_GAIN   |
+        BK4819_REG_30_DISABLE_MIC_ADC  |
+        BK4819_REG_30_ENABLE_TX_DSP    |
+        // BK4819_REG_30_DISABLE_TX_DSP    | // disables TX
+        BK4819_REG_30_DISABLE_RX_DSP);
+    
 }
 
 static void CW_EndPulse() {
-    BK4819_EnterTxMute();
+    // BK4819_EnterTxMute();
     BK4819_ToggleGpioOut(BK4819_GPIO5_PIN1_RED, false);
+
+    BK4819_WriteRegister(BK4819_REG_30,
+        BK4819_REG_30_ENABLE_VCO_CALIB |
+        BK4819_REG_30_ENABLE_UNKNOWN   |
+        BK4819_REG_30_DISABLE_RX_LINK  |
+        BK4819_REG_30_DISABLE_AF_DAC    | // disable beep
+        // BK4819_REG_30_ENABLE_AF_DAC    |
+        BK4819_REG_30_ENABLE_DISC_MODE |
+        BK4819_REG_30_ENABLE_PLL_VCO   |
+        // BK4819_REG_30_ENABLE_PA_GAIN   |
+        BK4819_REG_30_DISABLE_PA_GAIN   | // disable PA
+        BK4819_REG_30_DISABLE_MIC_ADC  |
+        BK4819_REG_30_ENABLE_TX_DSP    |
+        // BK4819_REG_30_DISABLE_TX_DSP    | // disables TX
+        BK4819_REG_30_DISABLE_RX_DSP);
+
+    // BK4819_SetupPowerAmplifier(0, 0);
+
 }
 
 static void CW_DisablePulse() {
@@ -110,16 +252,26 @@ static void CW_DisablePulse() {
     // BK4819_EnterTxMute();
     AUDIO_AudioPathOff();
 
+    BK4819_WriteRegister(BK4819_REG_30, 0xC1FE);
+    BK4819_SetupPowerAmplifier(0, 0);
+
+    BK4819_WriteRegister(BK4819_REG_70, 0);
+
+    // APP_EndTransmission();
+    // FUNCTION_Select(FUNCTION_FOREGROUND);
+    RADIO_SetupRegisters(true);
+    gFlagEndTransmission = false;
+
     // SYSTEM_DelayMs(5);
-    BK4819_TurnsOffTones_TurnsOnRX();
+    // BK4819_TurnsOffTones_TurnsOnRX();
     // SYSTEM_DelayMs(5);
     // BK4819_WriteRegister(BK4819_REG_71, ogToneConfig);
 
     if (gEnableSpeaker)
         AUDIO_AudioPathOn();
 
-    if (gCurrentFunction == FUNCTION_POWER_SAVE && gRxIdleMode)
-        BK4819_Sleep();
+    // if (gCurrentFunction == FUNCTION_POWER_SAVE && gRxIdleMode)
+    //     BK4819_Sleep();
 
     #ifdef ENABLE_VOX
         gVoxResumeCountdown = 80;
@@ -210,7 +362,7 @@ void CW_TimeSlice10ms() {
                         // next_tx = 0;
                         goto StartingDah;
                     } else {
-                        if (empty_queue > 10)
+                        if (empty_queue > 5)
                             cw_state = CW_QUEUE_EMPTY;
                         else
                             empty_queue++;
